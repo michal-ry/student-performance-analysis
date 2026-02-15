7252b13bede6b3ef5337622ec5e31e224a87ec64
michal-ry <ryziomichal@gmail.com>
2026-02-15 13:38:57 +0100

    Create COI view and add validation queries

--- sql_analysis/coi_analysis.sql
+++ sql_analysis/coi_analysis.sql
@@ -10,3 +10,32 @@ FROM student;
 SELECT *
 FROM student
 LIMIT 5;
+
+-- ==========================
+-- Creating COI View
+-- ==========================
+
+-- Creating View
+DROP VIEW IF EXISTS coi;
+
+CREATE VIEW coi AS
+SELECT
+guardian,
+nursery,
+famsup,
+paid,
+activities,
+internet,
+G3 AS g3
+FROM student;
+
+-- Total numbers of rows in coi view
+SELECT
+COUNT(*) AS coi_total_rows
+FROM coi;
+
+-- Preview records
+SELECT
+*
+FROM coi
+LIMIT 5;
